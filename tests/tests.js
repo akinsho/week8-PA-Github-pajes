ae078e9a64b66300e80d7a34f8a25c672b3bb26e
pbywater <philippabywater@gmail.com>
2017-04-12 13:12:00 +0100

    adding route testing and tidied up file
    relates to #15

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,54 @@ const test = require('tape');
 const server = require('../src/server.js');
 const data = require('../src/database/getdata.js');
 
+const routes = [{
+  method: 'GET',
+  url: '/',
+  expectedStatusCode: 200,
+}, {
+  method: 'GET',
+  url: '/write-post',
+  expectedStatusCode: 200,
+}, {
+  method: 'GET',
+  url: '/logged-in',
+  expectedStatusCode: 302,
+}, {
+  method: 'GET',
+  url: '/',
+  expectedStatusCode: 200,
+}, {
+  method: 'GET',
+  url: '/logged-out',
+  expectedStatusCode: 200,
+}, {
+  method: 'GET',
+  url: '/welcome',
+  expectedStatusCode: 302,
+}, {
+  method: 'POST',
+  url: '/submit-post',
+  expectedStatusCode: 200,
+}];
+
+
+routes.forEach((route) => {
+  test(`check ${route.url} route`, (t) => {
+    const { method, url, expectedStatusCode } = route;
+    const options = {
+      method,
+      url,
+    };
+    server.inject(options, (res) => {
+      t.equal(res.statusCode, expectedStatusCode, `Should return statuscode of ${expectedStatusCode}`);
+      if (route.url === '/' || route.url === '/write-post') {
+        t.equal(res.headers['content-type'], 'text/html; charset=utf-8', 'content-type should equal html');
+      }
+      t.end();
+    });
+  });
+});
+
 
 test('Passing test for travis', (t) => {
   t.equal(1, 1, '1 is equal to 1');
@@ -9,66 +57,8 @@ test('Passing test for travis', (t) => {
 });
 
 test('database functions', (t) => {
-  data.getBlogPosts((err, res)=>{
-    t.ok(res.length>1);
+  data.getBlogPosts((err, res) => {
+    t.ok(res.length > 1);
   });
   t.end();
 });
-
-test('Check home route', (t)=>{
-  var options = {
-    method: 'GET',
-    url: '/'
-  }
-  server.inject(options, (res)=>{
-    t.equal(res.statusCode, 200, 'Should return statuscode of 200');
-    t.equal(res.headers['content-type'], 'text/html; charset=utf-8');
-    t.end();
-  });
-})
-
-
-// test('Check  /my-posts', (t)=>{
-//   var options = {
-//     method: 'GET',
-//     url: '/my-posts'
-//   };
-//   server.inject(options, (res)=>{
-//     t.equal(res.statusCode, 200, 'Should return statuscode of 200');
-//     t.end();
-//   });
-// });
-
-// test('Check  /submit-post', (t)=>{
-//   var options = {
-//     method: 'POST',
-//     url: '/submit-post'
-//   }
-//   server.inject(options, (res)=>{
-//     t.equal(res.statusCode, 200, 'Should return statuscode of 200');
-//     t.end();
-//   });
-// });
-
-// test('Check  /logged-in', (t)=>{
-//   var options = {
-//     method: 'POST',
-//     url: '/logged-in'
-//   }
-//   server.inject(options, (res)=>{
-//     t.equal(res.statusCode, 200, 'Should return statuscode of 200');
-//     t.end();
-//   });
-// });
-
-test('Check  /write-post', (t)=>{
-  var options = {
-    method: 'GET',
-    url: '/write-post'
-  }
-  server.inject(options, (res)=>{
-    t.equal(res.statusCode, 200, 'Should return statuscode of 200');
-    t.equal(res.headers['content-type'], 'text/html; charset=utf-8', 'content-type should equal html');
-    t.end();
-  });
-})
